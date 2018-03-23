531d7a190f7b4ca6b015cb8da29526b1889dc229
germain72 <36983066+germain72@users.noreply.github.com>
2018-03-23 14:35:40 +0100

    V 1.0

--- mavenprojecthibernatetuto/src/main/java/ch/conceptforge/mavenprojecthibernatetuto/MaServletData.java
+++ mavenprojecthibernatetuto/src/main/java/ch/conceptforge/mavenprojecthibernatetuto/MaServletData.java
@@ -11,13 +11,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.hibernate.Session;
 
 /**
  *
  * @author Formation
  */
-public class ServletData extends HttpServlet {
+public class MaServletData extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -33,34 +32,16 @@ public class ServletData extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            Session sess = NewHibernateUtil.getSessionFactory().openSession();
-          
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ServletData</title><link href=\"css/cgestyle.css\" rel=\"stylesheet\" type=\"text/css\"/>");            
+            out.println("<title>Servlet MaServletData</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet ServletData at " + request.getContextPath() + "</h1>");
-            for(int i=1;i>0;i++)
-            {
-            Client ins = (Client) sess.get(Client.class,new Integer(i));
-            if(ins == null) break;
-            out.println("<hr/><div class='datarow'>Id : "+ins.getId()+"<br/>");
-            out.println("Nom : "+ins.getNom()+"<br/>");
-            out.println("Prenom : "+ins.getPrenom()+"<br/><hr/></div>");
-            }
-            out.println("<hr/><br/>");
-            for(Client oc: GestionClient.getAllClient())
-            {
-                out.println("<hr/><div class='datarow'>Id : "+oc.getId()+"<br/>");
-                out.println("Nom : "+oc.getNom()+"<br/>");
-                out.println("Prenom : "+oc.getPrenom()+"<br/><hr/></div>");
-            }
+            out.println("<h1>Servlet MaServletData at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
-            sess.close();
-        }   
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
