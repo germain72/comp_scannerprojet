871ce6e4b8aa3a28104798f6526796833da08ff5
Moi <Moi@LAPTOP-CLCIM1M6>
2018-03-25 13:25:41 +0200

    Modification de la classe Facteur

---
+++ mavenprojecthibernatetuto/src/main/resources/employee.sql
@@ -0,0 +1,59 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  Dim 25 mars 2018 à 13:24
+-- Version du serveur :  10.1.25-MariaDB
+-- Version de PHP :  5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `employee`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `employeedetails`
+--
+
+CREATE TABLE `employeedetails` (
+  `ID` int(11) NOT NULL,
+  `ename` varchar(50) NOT NULL,
+  `enumber` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `employeedetails`
+--
+ALTER TABLE `employeedetails`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `employeedetails`
+--
+ALTER TABLE `employeedetails`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
